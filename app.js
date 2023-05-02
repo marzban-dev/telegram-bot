061ff72bcc4fd621e161a5298d5dcbf35b3d3bdd
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-05-02 14:08:33 +0430

    Init

---
+++ app.js
@@ -0,0 +1,86 @@
+const {Telegraf} = require("telegraf");
+const dotenv = require("dotenv");
+const axios = require("axios");
+const {CalculateRemainingTime, downloadFile, getImages, searchImages} = require("./utils");
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
+bot.use(async (ctx, next) => {
+
+    next(ctx);
+});
+
+bot.start((ctx) => ctx.reply('Welcome'))
+
+bot.command("israel_deadline", ctx => {
+    const startDate = new Date("March 21, 2015");
+    const endDate = new Date("March 21, 2040");
+
+    const rTime = CalculateRemainingTime(startDate, endDate);
+
+    ctx.reply(`اسراییل ${rTime.years} سال و ${rTime.months} ماه و ${rTime.days} روز آینده را نخواهد دید`)
+});
+
+bot.on("message", async (ctx) => {
+
+    const msg = ctx.update.message.text;
+
+    if (msg && msg.includes("عکس بفرست از") && msg.split(" ").length >= 4) {
+
+        if (ctx.update.message.from.username === "Elliott_io") {
+            ctx.reply("تو یکی زجه نزن بدبخت", {
+                reply_to_message_id: ctx.message.message_id
+            })
+        } else {
+            const query = msg.split(" ").slice(3);
+
+            const images = await getImages({query: query.join(" "), count: 6});
+
+            if (images.length !== 0) {
+                await ctx.telegram.sendMediaGroup(ctx.chat.id, images.map(img => ({
+                    media: {url: img.url},
+                    type: "photo"
+                })), {
+                    reply_to_message_id: ctx.message.message_id
+                })
+            } else {
+                ctx.reply("چیزی نیافتم", {
+                    reply_to_message_id: ctx.message.message_id
+                })
+            }
+        }
+    }
+
+    if (msg && msg.includes("جستجو کن") && msg.split(" ").length >= 3) {
+
+        if (ctx.update.message.from.username === "Elliott_io") {
+            ctx.reply("تو یکی زجه نزن بدبخت", {
+                reply_to_message_id: ctx.message.message_id
+            })
+        } else {
+            const query = msg.split(" ").slice(2);
+
+            const images = await searchImages(query.join(" "));
+
+            if (images.length !== 0) {
+                await ctx.telegram.sendMediaGroup(ctx.chat.id, images.map(img => ({
+                    media: {url: img.url},
+                    type: "photo"
+                })), {
+                    reply_to_message_id: ctx.message.message_id
+                })
+            } else {
+                ctx.reply("چیزی نیافتم", {
+                    reply_to_message_id: ctx.message.message_id
+                })
+            }
+        }
+    }
+})
+
+bot.launch().then(() => console.log("Started"));
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
---
+++ utils.js
@@ -0,0 +1,99 @@
+const {Duration} = require("luxon");
+const axios = require("axios").default;
+const path = require("path");
+const fs = require("fs");
+
+exports.getImages = async (params = {count: 6}) => {
+
+    const options = {
+        method: 'GET',
+        url: ' https://api.unsplash.com/photos/random',
+        params: params,
+        headers: {
+            'Authorization': 'Client-ID qeTYKPHLaR41i_cnMRRqkPM002n_c8upwbWBVJSEcQg',
+        }
+    };
+
+    try {
+        const {data} = await axios.request(options);
+        return data.map(img => ({
+            id : img.id,
+            description : img.description,
+            url : img.urls.small
+        }));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+exports.searchImages = async (query,count = 6) => {
+
+    const options = {
+        method: 'GET',
+        url: ' https://api.unsplash.com/search/photos',
+        params: {
+            query,
+            page : 1,
+            per_page : count
+        },
+        headers: {
+            'Authorization': 'Client-ID qeTYKPHLaR41i_cnMRRqkPM002n_c8upwbWBVJSEcQg',
+        }
+    };
+
+    try {
+        const {data} = await axios.request(options);
+        return data.results.map(img => ({
+            id : img.id,
+            description : img.description,
+            url : img.urls.small
+        }));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+exports.downloadFile = async (fileUrl, downloadFolder) => {
+    // Get the file name
+    const fileName = path.basename(fileUrl);
+
+    // The path of the downloaded file on our machine
+    const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
+    try {
+        const response = await axios({
+            method: 'GET',
+            url: fileUrl,
+            responseType: 'stream',
+        });
+
+        const w = response.data.pipe(fs.createWriteStream(localFilePath));
+        w.on('finish', () => {
+            console.log('Successfully downloaded file!');
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+exports.CalculateRemainingTime = (createdDate, completionDate) => {
+    const currentDate = Date.now();
+
+    let _creationDate;
+    if (createdDate instanceof Date) _creationDate = createdDate.getTime(); else _creationDate = new Date(createdDate).getTime();
+
+    let _completionDate;
+    if (completionDate instanceof Date) _completionDate = completionDate.getTime(); else _completionDate = new Date(completionDate).getTime();
+
+    const readableDate = {};
+
+    const duration = Duration.fromMillis(_completionDate - currentDate).shiftTo('years', 'months', 'days', 'hours', 'minutes', 'seconds', 'milliseconds').toObject()
+
+    if (duration.years !== 0) readableDate["years"] = Math.abs(duration.years);
+    if (duration.months !== 0) readableDate["months"] = Math.abs(duration.months);
+    if (duration.days !== 0) readableDate["days"] = Math.abs(duration.days);
+    if (duration.hours !== 0) readableDate["hours"] = Math.abs(duration.hours);
+    if (duration.minutes !== 0) readableDate["minutes"] = Math.abs(duration.minutes);
+    readableDate["seconds"] = Math.abs(duration.seconds);
+
+    return readableDate;
+}
