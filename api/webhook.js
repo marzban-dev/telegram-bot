7869bce0169c621587d467b8d1d2662ee9b6eb7b
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-06-11 16:13:56 +0330

    Updated

--- api/webhook.js
+++ api/webhook.js
@@ -1,29 +1,15 @@
-// https://github.com/yagop/node-telegram-bot-api/issues/319#issuecomment-324963294
-// Fixes an error with Promise cancellation
 const dotenv = require("dotenv");
 const TelegramBot = require("node-telegram-bot-api");
-const { CalculateRemainingTime } = require("../utils");
+const { CalculateRemainingTime, Health } = require("../utils");
 dotenv.config();
 
 process.env.NTBA_FIX_319 = "test";
-
-// bot.start((ctx) => ctx.reply("Welcome"));
-
-// bot.command("test", async (ctx) => {
-//     ctx.reply("test");
-// });
-
-// bot.on("message", async (ctx) => {
-//     await ctx.reply("some message");
-// });
 
 module.exports = async (request, response) => {
     try {
         const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
         if (request.body && request.body.message) {
-            // Retrieve the ID for this chat
-            // and the text that the user sent
             const {
                 chat: { id, type },
                 text,
@@ -43,6 +29,11 @@ module.exports = async (request, response) => {
                 const message = `اسراییل ${rTime.years} سال و ${rTime.months} ماه و ${rTime.days} روز آینده را نخواهد دید`;
                 await bot.sendMessage(id, message, { parse_mode: "Markdown" });
             }
+
+            if (text.includes("/israel_health")) {
+                const health = Health();
+                await bot.sendMessage(id, health, { parse_mode: "Markdown" });
+            }
         }
     } catch (error) {
         // can log it into the Vercel console
@@ -52,41 +43,3 @@ module.exports = async (request, response) => {
 
     response.send("ok");
 };
-
-// export async function handleTestCommand(ctx: TelegrafContext) {
-//   const COMMAND = "/test"
-//   const { message } = ctx
-
-//   let reply = "Hello there! Awaiting your service"
-
-//   const didReply = await ctx.reply(reply, {
-//     reply_to_message_id: message?.message_id,
-//   })
-
-//   if (didReply) {
-//     console.log(`Reply to ${COMMAND} command sent successfully.`)
-//   } else {
-//     console.error(
-//       `Something went wrong with the ${COMMAND} command. Reply not sent.`
-//     )
-//   }
-// }
-
-// export async function handleOnMessage(ctx: TelegrafContext) {
-//   const { message } = ctx
-
-//   const isGroup =
-//     message?.chat.type === "group" || message?.chat.type === "supergroup"
-
-//   if (isGroup) {
-//     await ctx.reply("This bot is only available in private chats.")
-//     return
-//   }
-
-//   const telegramUsername = message?.from?.username
-//   const reply = "a message was sent"
-
-//   await ctx.reply(reply, {
-//     reply_to_message_id: message.message_id,
-//   })
-// }
--- utils.js
+++ utils.js
@@ -1,57 +1,55 @@
-const {Duration} = require("luxon");
+const { Duration } = require("luxon");
 const axios = require("axios").default;
 const path = require("path");
 const fs = require("fs");
 
-exports.getImages = async (params = {count: 6}) => {
-
+exports.getImages = async (params = { count: 6 }) => {
     const options = {
-        method: 'GET',
-        url: ' https://api.unsplash.com/photos/random',
+        method: "GET",
+        url: " https://api.unsplash.com/photos/random",
         params: params,
         headers: {
-            'Authorization': 'Client-ID qeTYKPHLaR41i_cnMRRqkPM002n_c8upwbWBVJSEcQg',
-        }
+            Authorization: "Client-ID qeTYKPHLaR41i_cnMRRqkPM002n_c8upwbWBVJSEcQg",
+        },
     };
 
     try {
-        const {data} = await axios.request(options);
-        return data.map(img => ({
-            id : img.id,
-            description : img.description,
-            url : img.urls.small
+        const { data } = await axios.request(options);
+        return data.map((img) => ({
+            id: img.id,
+            description: img.description,
+            url: img.urls.small,
         }));
     } catch (e) {
         console.log(e);
     }
-}
+};
 
-exports.searchImages = async (query,count = 6) => {
-
+exports.searchImages = async (query, count = 6) => {
     const options = {
-        method: 'GET',
-        url: ' https://api.unsplash.com/search/photos',
+        method: "GET",
+        url: " https://api.unsplash.com/search/photos",
         params: {
             query,
-            page : 1,
-            per_page : count
+            page: 1,
+            per_page: count,
         },
         headers: {
-            'Authorization': 'Client-ID qeTYKPHLaR41i_cnMRRqkPM002n_c8upwbWBVJSEcQg',
-        }
+            Authorization: "Client-ID qeTYKPHLaR41i_cnMRRqkPM002n_c8upwbWBVJSEcQg",
+        },
     };
 
     try {
-        const {data} = await axios.request(options);
-        return data.results.map(img => ({
-            id : img.id,
-            description : img.description,
-            url : img.urls.small
+        const { data } = await axios.request(options);
+        return data.results.map((img) => ({
+            id: img.id,
+            description: img.description,
+            url: img.urls.small,
         }));
     } catch (e) {
         console.log(e);
     }
-}
+};
 
 exports.downloadFile = async (fileUrl, downloadFolder) => {
     // Get the file name
@@ -61,14 +59,14 @@ exports.downloadFile = async (fileUrl, downloadFolder) => {
     const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
     try {
         const response = await axios({
-            method: 'GET',
+            method: "GET",
             url: fileUrl,
-            responseType: 'stream',
+            responseType: "stream",
         });
 
         const w = response.data.pipe(fs.createWriteStream(localFilePath));
-        w.on('finish', () => {
-            console.log('Successfully downloaded file!');
+        w.on("finish", () => {
+            console.log("Successfully downloaded file!");
         });
     } catch (err) {
         throw new Error(err);
@@ -79,14 +77,18 @@ exports.CalculateRemainingTime = (createdDate, completionDate) => {
     const currentDate = Date.now();
 
     let _creationDate;
-    if (createdDate instanceof Date) _creationDate = createdDate.getTime(); else _creationDate = new Date(createdDate).getTime();
+    if (createdDate instanceof Date) _creationDate = createdDate.getTime();
+    else _creationDate = new Date(createdDate).getTime();
 
     let _completionDate;
-    if (completionDate instanceof Date) _completionDate = completionDate.getTime(); else _completionDate = new Date(completionDate).getTime();
+    if (completionDate instanceof Date) _completionDate = completionDate.getTime();
+    else _completionDate = new Date(completionDate).getTime();
 
     const readableDate = {};
 
-    const duration = Duration.fromMillis(_completionDate - currentDate).shiftTo('years', 'months', 'days', 'hours', 'minutes', 'seconds', 'milliseconds').toObject()
+    const duration = Duration.fromMillis(_completionDate - currentDate)
+        .shiftTo("years", "months", "days", "hours", "minutes", "seconds", "milliseconds")
+        .toObject();
 
     if (duration.years !== 0) readableDate["years"] = Math.abs(duration.years);
     if (duration.months !== 0) readableDate["months"] = Math.abs(duration.months);
@@ -96,4 +98,16 @@ exports.CalculateRemainingTime = (createdDate, completionDate) => {
     readableDate["seconds"] = Math.abs(duration.seconds);
 
     return readableDate;
-}
+};
+
+exports.Health = () => {
+    const endDate = new Date("March 21, 2040").getTime() / 1000 / 60 / 60 / 24 / 365;
+    const currentDate = new Date().getTime() / 1000 / 60 / 60 / 24 / 365;
+
+    const progress = Array.from({ length: Math.floor(endDate - currentDate) })
+        .map(() => "=")
+        .join("");
+    const fullProgress = Array.from({ length: 25 }).map(() => "-");
+    fullProgress.splice(0, progress.length);
+    return "Israel Health [" + progress + fullProgress.join("") + "]";
+};
